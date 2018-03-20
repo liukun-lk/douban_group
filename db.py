0e5c6151a8b95f35d645938398df466a55de2453
liukun-lk <f221mix@gmail.com>
2018-03-20 22:18:06 +0800

    add topic and topiclist db table

--- db.py
+++ db.py
@@ -1,20 +1,42 @@
 #! /usr/bin/env python3
-
 from peewee import *
+import datetime
 
 db = SqliteDatabase('test.db')
 
-class Person(Model):
-    name = CharField()
-    birthday = DateField()
-    is_relative = BooleanField()
+
+class TopicList(Model):
+    title = CharField()
+    author = CharField()
+    reply = IntegerField()
+    last_reply_time = DateTimeField(default=datetime.datetime.now)
+    url = CharField()
+    crawled_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+    topic_id = IntegerField()
 
     class Meta:
         database = db
 
+
+class Topic(Model):
+    title = CharField()
+    url = CharField()
+    crawled_at = DateTimeField(default=datetime.datetime.now)
+    create_time = DateTimeField(default=datetime.datetime.now)
+    author = CharField()
+    content = TextField()
+    images = TextField()
+
+    class Meta:
+        database = db
+
+
 def init_table():
     db.connect()
-    db.create_tables([Person])
+    db.create_tables([TopicList, Topic])
+    db.close()
+
 
 if __name__ == '__main__':
     init_table()
